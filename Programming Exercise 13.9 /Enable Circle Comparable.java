636c2a4d56f411834a8b1875e475dde9be3affc9
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-06-21 22:27:49 -0400

    Enable Circle Comparable.java

--- Programming Exercise 13.9 /Enable Circle Comparable.java
+++ Programming Exercise 13.9 /Enable Circle Comparable.java
@@ -1,35 +1,63 @@
+/**
+ * The Circle class represents a geometric circle with a radius.
+ * It extends GeometricObject and implements Comparable for comparing circles by area.
+ */
 public class Circle extends GeometricObject implements Comparable<Circle> {
+  private double radius;
+
+  /**
+   * Constructs a Circle with radius 0.0.
+   */
   public Circle() {
     this(0.0);
   }
 
+  /**
+   * Constructs a Circle with the specified radius.
+   * @param radius the radius of the circle
+   */
   public Circle(double radius) {
-    this.radius = radius;
+    setRadius(radius);
   }
 
+  /**
+   * Returns the radius of the circle.
+   * @return the radius
+   */
   public double getRadius() {
     return radius;
   }
 
+  /**
+   * Sets the radius of the circle.
+   * @param radius the new radius
+   * @throws IllegalArgumentException if radius is negative
+   */
   public void setRadius(double radius) {
     if (radius < 0) throw new IllegalArgumentException("Radius cannot be negative");
     this.radius = radius;
   }
 
+  /**
+   * Returns the area of the circle.
+   * @return the area
+   */
   public double getArea() {
     return radius * radius * Math.PI;
   }
 
+  /**
+   * Returns a string representation of the circle.
+   * @return a string describing the circle
+   */
   @Override
   public String toString() {
     return "Circle(radius = " + radius + ")";
   }
 
-  @Override
-  public double getPerimeter() {
-    return 2 * Math.PI * radius;
-  }
-
+  /**
+   * @return 1 if this is larger, -1 if smaller, 0 if equal
+   */
   @Override
   public int compareTo(Circle o) {
     if (this.getArea() > o.getArea()) {
@@ -41,6 +69,11 @@ public class Circle extends GeometricObject implements Comparable<Circle> {
     }
   }
 
+  /**
+   * Checks if this circle is equal to another object.
+   * @param obj the object to compare
+   * @return true if equal, false otherwise
+   */
   @Override
   public boolean equals(Object obj) {
     if (this == obj) return true;
@@ -49,11 +82,19 @@ public class Circle extends GeometricObject implements Comparable<Circle> {
     return Double.compare(circle.radius, radius) == 0;
   }
   
+  /**
+   * Returns the hash code for this circle.
+   * @return the hash code
+   */
   @Override
   public int hashCode() {
     return Double.hashCode(radius);
   }
   
+  /**
+   * Main method for testing the Circle class.
+   * @param args command line arguments
+   */
   public static void main(String[] args) {
     Circle o1 = new Circle(10.0);
     Circle o2 = new Circle(5.0);
@@ -63,4 +104,13 @@ public class Circle extends GeometricObject implements Comparable<Circle> {
     System.out.println("o1.equals(o2): " + o1.equals(o2));
     System.out.println("o1.compareTo(o2): " + o1.compareTo(o2));
   }
+}
+
+/**
+ * A minimal GeometricObject class to allow Circle to compile.
+ * You should replace this with your actual GeometricObject implementation if available.
+ */
+class GeometricObject {
+  public double getPerimeter() { return 0.0; }
+  public double getArea() { return 0.0; }
 }
